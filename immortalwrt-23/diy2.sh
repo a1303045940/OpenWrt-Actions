7b1d4bafdb19bb3d3e83ef7f307aecc3c22d6f50
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-03-05 00:21:44 +0800

    Update diy2.sh

--- immortalwrt-23/diy2.sh
+++ immortalwrt-23/diy2.sh
@@ -11,9 +11,9 @@
 #
 
 # 修改默认IP地址
-sed -i "s/192.168.1.1/192.168.3.1/g" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.6.1/g" package/base-files/files/bin/config_generate
 # 修改设备说明
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='a130304540 $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
 # OpenClash
 mkdir -p files/etc/openclash/core
 # CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
