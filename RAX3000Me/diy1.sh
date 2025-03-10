0dc1bc576c9aebf9ae1461c040275cb3401e038d
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-03-10 22:22:00 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -28,5 +28,11 @@ mv luci-app-easytier/easytier package/easytier
 mv luci-app-easytier/luci-app-easytier package/luci-app-easytier
 rm -rf luci-app-easytier
 ##添加自己的插件库
-#sed -i '$a src-git mzwrt_package https://github.com/mzwrt/mzwrt_package' feeds.conf.default
-#git clone --depth 1 -b main https://github.com/mzwrt/mzwrt_package package/openwrt-packages
+# sed -i '$a src-git mzwrt_package https://github.com/mzwrt/mzwrt_package' feeds.conf.default
+# git clone --depth 1 -b main https://github.com/mzwrt/mzwrt_package package/openwrt-packages
+# 科学上网插件
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
