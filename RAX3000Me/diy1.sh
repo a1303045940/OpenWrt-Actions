262c46afafaa8eab81ad0056718309f27a9dfb46
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-03-10 22:20:33 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -12,10 +12,10 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 # 自定义软件源
-#git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
+git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
 # Mosdns
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
@@ -23,10 +23,10 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.d
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # Easytier
-#git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git luci-app-easytier
-#mv luci-app-easytier/easytier package/easytier
-#mv luci-app-easytier/luci-app-easytier package/luci-app-easytier
-#rm -rf luci-app-easytier
+git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git luci-app-easytier
+mv luci-app-easytier/easytier package/easytier
+mv luci-app-easytier/luci-app-easytier package/luci-app-easytier
+rm -rf luci-app-easytier
 ##添加自己的插件库
 #sed -i '$a src-git mzwrt_package https://github.com/mzwrt/mzwrt_package' feeds.conf.default
 #git clone --depth 1 -b main https://github.com/mzwrt/mzwrt_package package/openwrt-packages
