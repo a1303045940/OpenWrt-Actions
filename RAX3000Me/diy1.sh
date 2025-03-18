79aba5bd3032fdf71e315b91a4aaca4133f90720
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-03-18 23:19:40 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -8,40 +8,36 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-#echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
+
 
 # 自定义软件源
 git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
 # Mosdns
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-#rm -rf feeds/packages/net/v2ray-geodata
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# 移除要替换的包
+rm -rf feeds/packages/lang/golang
+# golong1.24依赖
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# rm -rf feeds/packages/net/v2ray-geodata
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # Easytier
 git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git luci-app-easytier
 mv luci-app-easytier/easytier package/easytier
 mv luci-app-easytier/luci-app-easytier package/luci-app-easytier
 rm -rf luci-app-easytier
 # 科学上网插件
-# rm -rf {*passwall*,*bypass*,*homeproxy*,*mihomo*}
+rm -rf {*passwall*,*bypass*,*homeproxy*,*mihomo*}
 # rm -rf feeds/packages/lang/golang
-# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 ##添加自己的插件库
-#git clone --depth 1 -b main https://github.com/xiaorouji/openwrt-passwall-packages.git luci-app
-#mv luci-app/openwrt-passwall package/openwrt-passwall
-#rm -rf openwrt-passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-# git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
+git clone --depth 1 -b main https://github.com/xiaorouji/openwrt-passwall-packages.git luci-app
+mv luci-app/openwrt-passwall package/openwrt-passwall
+rm -rf openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 
 #git clone https://github.com/sbwml/luci-app-mosdns.git package/mosdns
